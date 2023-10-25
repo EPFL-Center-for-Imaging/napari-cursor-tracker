fdbf612b63c8d45ccf3b2b6d51b65169646f550a
faymanns <florian.aymanns@t-online.de>
2023-10-25 14:30:40 +0200

    Setup automatic versioning with git tags.

--- src/napari_cursor_tracker/__init__.py
+++ src/napari_cursor_tracker/__init__.py
@@ -1,8 +1,9 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 from ._sample_data import make_sample_data
 from ._widget import CursorTracker
-
-__version__ = "0.0.1"
-
 
 __all__ = (
     "make_sample_data",
